e6097c5f14520d1768bb2972959287d2c17c93cf
wanghaixin <18811442527@163.com>
2017-01-04 16:52:05 +0800

    进入主页显示所有的产品排行榜

--- src/main/webapp/scriptjs/home.js
+++ src/main/webapp/scriptjs/home.js
@@ -45,49 +45,68 @@ jQuery(document).ready(function() {	//首先渲染
     //contentType: "application/json; charset=utf-8",
     data:{foodType:foodType},
     success: function (data) {
-      var stringfortrlist = "";
-      for (var i = 0; i < data.data.length; i++) {
-        var idforlog=i+1;//加了1的，要减去
-        var textforOperateButton = (data.data[i].status==0)?"开启":"关闭";
-        var nodeStatus;
-        var nodeType;
-        if(data.data[i].status==0){
-          nodeStatus="关闭";
-        }else if(data.data[i].status==1){
-          nodeStatus="活跃";
-        }else{
-          nodeStatus="空闲";
-        }
-        if(data.data[i].type==0){
-          nodeType="Binary";
-        }else if(data.data[i].type==1){
-          nodeType="Java";
-        }else{
-          nodeType="Python";
-        }
-        var stringForConvert=data.data[i].nodeId+"&"+data.data[i].taskAmount+"&"+idforlog+"&"+textforOperateButton;
-        var stringfortr ="<tr class=\"gradeX\">"+
-            "<td class=\"center\"><input type=\"checkbox\" name=\"checkList\"></td>"+
-            "<td class=\"center\">"+idforlog+"</td>"+
-            "<td class=\"center\">"+data.data[i].nodeId+"</td>"+
-            "<td class=\"center\">"+nodeType+"</td>"+
-            "<td class=\"center\">"+data.data[i].nodeName+"</td>"+
-            "<td class=\"center\">"+userName+"</td> <td class=\"center\" id='"+data.data[i].nodeId+"'>"+nodeStatus+"</td>"+
-            "<td class=\"hidden-xs\"><a onclick='changeToTaskView(this)' id='"+data.data[i].nodeId+"' class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+data.data[i].taskAmount+"</a></td>"+
-            "<td class=\"center\"><a onclick='showtheHisTask(this)'  id='"+data.data[i].nodeId+"' class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+data.data[i].historyTaskAmount+"</a></td>"+
-            "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">详情</a></td>"+
-            "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">详情</a></td>"+
-            "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">详情</a></td>"+
-            "<td class=\"center hidden-xs\"><a onclick='showTheWarnModal(this)' id="+stringForConvert+" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+textforOperateButton+"</a></td>"+
-            "</tr>";
+      var stringfortrlist = "<div class='tc-ch wow fadeInDown' data-wow-duration='.8s' data-wow-delay='.2s'> <div class='tch-img'>"+
+      "<a href='singlepage.html'><img src='images/t4.jpg' class='img-responsive' alt=''></a> </div>"+
+      "   <h3><a href='singlepage.html'>Lorem Ipsum is simply</a></h3>"+
+      "<h6>BY <a href='singlepage.html'>ADAM ROSE </a>JULY 10 2016.</h6> <p>Top1</p> <div class='bht1'>"+
+      "   <a href='#table-modal-new-task data-toggle='modal' class='reload' id='create_task_button'>查看详情</a> </div> <div class='soci'> </div>"+
+      "   <div class='clearfix'></div> </div> <div class='clearfix'></div>";
+
+      for (var i = 0; i < data.data.length; i++)
+      {
+        var stringfortr = "<div class='wthree'>"+
+           " <div class='col-md-6 wthree-left wow fadeInDown'  data-wow-duration='.8s' data-wow-delay='.2s'> <div class='tch-img'>"+
+           " <a href='singlepage.html'><img src='images/"+data.data[i].imageName+"'class='img-responsive' alt=''></a> </div> </div>"+
+        "<div class='col-md-6 wthree-right wow fadeInDown'  data-wow-duration='.8s' data-wow-delay='.2s'>"+
+        "  <h3><a href='singlepage.html'>"+data.data[i].foodName+"</a></h3>"+
+        " <h6><a href='singlepage.html'>"+data.data[i].address+"</a></h6> <p>人均"+data.data[i].averageMoney+"元</p>"+
+        " <div class='bht1'>"+
+        "  <a href='table-modal-new-task' data-toggle='modal' class='reload' id='create_task_button'>查看详情</a> </div>"+
+        "  <div class='soci'> <ul>"+
+        "  <li><a  style='border: none' href=''><i class='glyphicon glyphicon-heart'></i></a></li>"+
+        "</ul> </div> <div class='clearfix'></div> </div> <div class='clearfix'></div> </div>"
+
         stringfortrlist = stringfortrlist + stringfortr;
+        //var idforlog=i+1;//加了1的，要减去
+        //var textforOperateButton = (data.data[i].status==0)?"开启":"关闭";
+        //var nodeStatus;
+        //var nodeType;
+        //if(data.data[i].status==0){
+        //  nodeStatus="关闭";
+        //}else if(data.data[i].status==1){
+        //  nodeStatus="活跃";
+        //}else{
+        //  nodeStatus="空闲";
+        //}
+        //if(data.data[i].type==0){
+        //  nodeType="Binary";
+        //}else if(data.data[i].type==1){
+        //  nodeType="Java";
+        //}else{
+        //  nodeType="Python";
+        //}
+        //var stringForConvert=data.data[i].nodeId+"&"+data.data[i].taskAmount+"&"+idforlog+"&"+textforOperateButton;
+        //var stringfortr ="<tr class=\"gradeX\">"+
+        //    "<td class=\"center\"><input type=\"checkbox\" name=\"checkList\"></td>"+
+        //    "<td class=\"center\">"+idforlog+"</td>"+
+        //    "<td class=\"center\">"+data.data[i].nodeId+"</td>"+
+        //    "<td class=\"center\">"+nodeType+"</td>"+
+        //    "<td class=\"center\">"+data.data[i].nodeName+"</td>"+
+        //    "<td class=\"center\">"+userName+"</td> <td class=\"center\" id='"+data.data[i].nodeId+"'>"+nodeStatus+"</td>"+
+        //    "<td class=\"hidden-xs\"><a onclick='changeToTaskView(this)' id='"+data.data[i].nodeId+"' class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+data.data[i].taskAmount+"</a></td>"+
+        //    "<td class=\"center\"><a onclick='showtheHisTask(this)'  id='"+data.data[i].nodeId+"' class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+data.data[i].historyTaskAmount+"</a></td>"+
+        //    "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">详情</a></td>"+
+        //    "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">详情</a></td>"+
+        //    "<td class=\"center hidden-xs\"><a href=\"#table-modal-showVelocity\" data-toggle=\"modal\" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">详情</a></td>"+
+        //    "<td class=\"center hidden-xs\"><a onclick='showTheWarnModal(this)' id="+stringForConvert+" class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">"+textforOperateButton+"</a></td>"+
+        //    "</tr>";
+        //stringfortrlist = stringfortrlist + stringfortr;
       }
-      $("#datatableForNode").dataTable().fnDestroy();
-      $("#datatableForTask").dataTable().fnDestroy();
-      $('#tbodyfornodelist').html(stringfortrlist);
-      AutoCheckLang();
-      $("#datatableForTask").css("width","100%");
-      $("#CPCEP_id").text(userName+"的节点列表信息");
+
+      $('#showheatList').html(stringfortrlist);
+      //AutoCheckLang();
+      //$("#datatableForTask").css("width","100%");
+      //$("#CPCEP_id").text(userName+"的节点列表信息");
     }
   });
 })
